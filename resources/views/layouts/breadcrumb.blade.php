b9fc8114c45b1ec6782bc3997da26de75929b8f3
sitirosanti <rosantiagustina85@gmail.com>
2020-07-25 14:57:03 +0700

    pertemuan 10

---
+++ resources/views/layouts/breadcrumb.blade.php
@@ -0,0 +1,9 @@
+<nav aria-label="breadcrumb" class="mt-2">
+    <ol class="breadcrumb">
+        <li class="breadcrum-item"><a href="#">Home</a></li>
+        <li class="breadcrum-item"><a href="#">@yield('bread1')</a></li>
+        <li class="breadcrum-item active"aria-current="page">@yield('bread2')</li>
+    </ol>
+</nav>
+
+        
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('tittle','HalamanMahasiswa')
+
+@section('bread1','Mahasiswa')
+@section('bread2','Mahasiswa')
+
+@section('content')
+    <p>paragraf</p>
+
+@endsection
